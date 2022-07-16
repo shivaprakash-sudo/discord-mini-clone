d39a68c042cdacf4c1ed356c01b7ac82a86e5b54
Shiva Prakash Pendem <pendemsivaprakash@gmail.com>
2022-07-16 17:16:39 +0100

    add sidebar hr

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -5,6 +5,7 @@ const SideBar = () => {
     return (
         <div className="flex flex-col fixed top-0 left-0 h-screen w-16 m-0 bg-gray-900 text-white shadow-lg">
             <SideBarIcon icon={<FaDiscord size={28} />} text="Dashboard" />
+            <hr className="sidebar-hr" />
             <SideBarIcon icon={<FaCode size={28} />} text="Coding" />
             <SideBarIcon icon={<FaBook size={28} />} text="Reading" />
             <SideBarIcon icon={<MdPerson size={28} />} text="Friends" />
