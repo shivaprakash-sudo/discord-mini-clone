45db8b6f28bfec80aa8870fe16f9050ac100e93f
Shiva Prakash Pendem <pendemsivaprakash@gmail.com>
2022-07-16 14:21:34 +0100

    discord sidebar complete

---
+++ src/SideBar.js
@@ -0,0 +1,26 @@
+import { FaDiscord, FaBook, FaCode, FaPlus } from 'react-icons/fa';
+import { MdPerson } from "react-icons/md";
+
+const SideBar = () => {
+    return (
+        <div className="flex flex-col fixed top-0 left-0 h-screen w-16 m-0 bg-gray-900 text-white shadow-lg">
+            <SideBarIcon icon={<FaDiscord size={28} />} text="Dashboard" />
+            <SideBarIcon icon={<FaCode size={28} />} text="Coding" />
+            <SideBarIcon icon={<FaBook size={28} />} text="Reading" />
+            <SideBarIcon icon={<MdPerson size={28} />} text="Friends" />
+            <SideBarIcon icon={<FaPlus size={28} />} text="Add new server" />
+        </div>
+    );
+}
+
+const SideBarIcon = ({ icon, text = "tooltip 💡" }) => {
+    return (
+        <div className="sidebar-icon group">
+            {icon}
+            <span className='sidebar-tooltip group-hover:scale-100'>{text}</span>
+        </div>
+    );
+}
+
+
+export default SideBar;
